2610e0eefe3b4aed63ced0d165d74b0d3e2711a2
moguranran <peehssalg@gmail.com>
2019-12-15 16:36:59 +0900

    index.htmlとviewsでテンプレートの作成まで完了

--- DjangoWebProject1/HelloDjangpApp/views.py
+++ DjangoWebProject1/HelloDjangpApp/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
-
 def index(request):
-    return HttpResponse("Hello Django!")
+    now = datetime.now()
+
+    return render(request,
+        "HelloDjangpApp\index.html",
+        {
+            'title':"Hello Django",
+            'message':"Hello Django",
+            'content':" on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+)
+
+
+#def index(request):
+#    now = datetime.now()
+
+#    html_content = "<html><head><title>Hello, Django</title></head><body>"
+#    html_content += "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
+#    html_content += "</body></html>"
+
+#    return HttpResponse(html_content)
